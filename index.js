e5c838ecd534afd21cf57176af1ec9d120756aa6
Kalvine <kkalvine4@gmail.com>
2024-06-29 00:05:36 +0300

    Adds Object Methods

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+const employee = {
+    name: `Sherlock Holmes`,
+    streetAddress: `21B Baker Street`,
+}
+
+console.log(deleteFromEmployeeByKey(employee, name))
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+   return {
+    ...obj,
+    [key]: value,
+   };   
+}
+
+//console.log(updateEmployeeWithKeyAndValue(employee, `John Watson`, `21A Baker Street`))
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+          obj[key]=value;
+          return obj;
+}
+
+//console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, `John Watson`, `21A Baker Street`))
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj2 = {...obj[key]}
+    delete newObj2[key]
+    return newObj2;
+}
+
+//console.log(deleteFromEmployeeByKey(employee, name))
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
+
+//console.log(destructivelyDeleteFromEmployeeByKey(employee, name))
